c6feb91cde05d3de7ee087eedd169693a6b0e07a
Gopi <gopi.iam29@gmail.com>
2026-01-02 18:12:22 +0530

    setSession for cookie

--- appwrite.js
+++ appwrite.js
@@ -3,10 +3,10 @@ import { Client, Account } from "appwrite";
 const { VITE_PROJECT_ID, VITE_ENDPOINT } = import.meta.env;
 console.log(VITE_PROJECT_ID, VITE_ENDPOINT);
 
-const client = new Client()
-    .setEndpoint(VITE_ENDPOINT)
-    .setProject(VITE_PROJECT_ID);
-
+const client = new Client();
+    client.setEndpoint(import.meta.env.VITE_ENDPOINT);
+    client.setProject(import.meta.env.VITE_PROJECT_ID);
+    client.setSession();
 const account = new Account(client);
 
 export { account };
